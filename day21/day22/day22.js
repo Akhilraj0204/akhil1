2544e3dfc822439e75eba2e3a0ea4ccd824916dd
Akhilraj0204 <cherriakhilraj2117@gmail.com>
2025-11-06 18:28:00 +0530

    Add files via upload

---
+++ day21/day22/day22.js
@@ -0,0 +1,50 @@
+//  Using Const 
+
+var city = "Hyderabad";
+let age = 30;
+//const country = "India";
+
+
+var city = "Banglore";
+age = 35;            
+//  country = "thailand"; 
+
+age = 40;
+
+let village = "Bankok"
+
+let Akhil = "hello team";
+
+
+console.log(village);
+
+
+//const country = "russia";
+
+console.log(country);
+
+
+let cityes = "banglore";
+//let age = 25;
+let country = "India";
+
+let1 = "akhil"
+let2 = "raj"
+
+console.log(city);
+console.log(age);
+console.log(country);
+
+console,log(let1*let2)
+
+var city = "hyderabad";
+var age = "30";
+var country = "India";
+
+var city = "Banglore"
+var age = "40"
+var country = "hongkong"
+
+let village = "Tadipatry" 
+
+console.log(village);
